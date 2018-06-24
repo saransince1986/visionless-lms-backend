31e4a2549708f550501ff661f124c4e588f694f6
cefeboru <cefeboru@gmail.com>
2018-06-23 23:20:37 -0600

    Adding remaining field to user model

--- src/models/user.js
+++ src/models/user.js
@@ -2,11 +2,16 @@
 module.exports = (sequelize, DataTypes) => {
   var user = sequelize.define('user', {
     email: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING
+    phoneNumber: DataTypes.STRING,
+    authId: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    midName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    secondLastName: DataTypes.STRING,
+    idNumber: DataTypes.STRING
   }, {});
   user.associate = function(models) {
-    // associations can be defined here
-    user.belongsTo(models.role)
+    user.belongsTo(models.role);
   };
   return user;
 };
