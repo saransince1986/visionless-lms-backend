d277f06cb1a909ebc4d5c634258ed398ef88ff83
cefeboru <cefeboru@gmail.com>
2018-06-24 17:57:05 -0600

    Using morgan and body parser middlewares

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-'use strict';
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import routes from './routes';
 
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
+const PORT = process.env.PORT || '8080';
+const HOST = process.env.HOST || '0.0.0.0';
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', routes);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
