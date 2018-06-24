5bb05e3c8ab5f5650dd5af99aac2c45626febc5f
cefeboru <cefeboru@gmail.com>
2018-06-24 17:55:55 -0600

    Registering request handlers and router into the container

--- src/config/awilixContainer.js
+++ src/config/awilixContainer.js
@@ -1,14 +1,28 @@
-import awilix from 'awilix';
+import { createContainer, Lifetime, asValue, asClass, asFunction } from 'awilix';
 import db from '../models';
 
-const container = awilix.createContainer();
+const container = createContainer();
 
 Object.keys(db).forEach((model) => {
   if (model.toLowerCase() !== 'sequelize') {
-    container.registerValue(`${model}Model`, db[model]);
+    container.register(`${model}Model`, asValue(db[model]));
   }
 });
 
+container.loadModules([`${__dirname}/../domain/request-handlers/!(index).js`], {
+  formatName: (name) => `${name}RequestHandler`,
+  resolverOptions: {
+    lifetime: Lifetime.SINGLETON,
+    register: asClass,
+  },
+});
+
+container.loadModules([`${__dirname}/../routes/!(index).js`], {
+  formatName: (name) => `${name}Router`,
+  resolverOptions: {
+    lifetime: Lifetime.SINGLETON,
+    register: asFunction,
+  },
+});
+
 export default container;
-
-
