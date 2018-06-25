612a5e02c39e287315fffc69c81a19538a62f5af
cefeboru <cefeboru@gmail.com>
2018-06-24 22:15:07 -0600

    Error handling middleware for joi

--- src/server.js
+++ src/server.js
@@ -11,6 +11,11 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
-
+app.use((err, req, res, next) => {
+  if (err.isBoom) {
+    res.status(err.output.statusCode).send(err.output.payload);
+  }
+  next(err);
+});
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
