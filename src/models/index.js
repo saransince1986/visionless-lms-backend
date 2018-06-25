01ff43954593f2dd7465ef8bbc4922afdf4e0cce
cefeboru <cefeboru@gmail.com>
2018-06-24 22:16:19 -0600

    Require was broken

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/sequelize.json')[env];
+const config = require('../config/sequelize.js')[env];
 
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
