0ba26dfa25fbe778bcbfa6e13202b67194c537b8
cefeboru <cefeboru@gmail.com>
2018-07-03 23:11:31 -0600

    Breaking out payload

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import expressJoi from 'express-joi-validator';
+import { isEmpty } from 'lodash';
 
 export default function ({
   usersRequestHandler,
@@ -16,21 +17,38 @@ export default function ({
       res.send([]);
     }
   });
+
   router.get('/:userId', async (req, res) => {
-    const { userId } = req.params;
-    const user = await usersRequestHandler.getUserById({ userId });
-    if (user) {
+    const user = await usersRequestHandler.getUserById(req.params.userId);
+    if (!isEmpty(user)) {
       res.send(user);
     } else {
       res.statusCode = 404;
       res.send([]);
     }
   });
+
+  router.delete('/:userId', async (req, res) => {
+    await usersRequestHandler.deleteUserById(req.params.userId)
+      .then(() => res.send('OK'));
+  });
+
   router.post('/', expressJoi(usersSchemaValidator.post), async (req, res) => {
     try {
-      const user = await usersRequestHandler.createUser(req.body);
+      const user = await usersRequestHandler.createUser({
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        authId: req.body.authId,
+        firstName: req.body.firstName,
+        middleName: req.body.middleName,
+        lastName: req.body.lastName,
+        secondLastName: req.body.secondLastName,
+        idNumber: req.body.idNumber,
+        role: req.body.role,
+      });
       res.send(user);
     } catch (err) {
+      console.error(err);
       res.statusCode = 500;
       res.send(err.stack);
     }
