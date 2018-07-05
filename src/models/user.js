380357f2f34eb5fe9db06798554fc3cb6befa21b
cefeboru <cefeboru@gmail.com>
2018-07-04 21:04:15 -0600

    Associating user with course through enrollments table

--- src/models/user.js
+++ src/models/user.js
@@ -15,6 +15,9 @@ module.exports = (sequelize, DataTypes) => {
     user.belongsToMany(models.disability, {
       through: 'usersDisabilities'
     });
+    user.belongsToMany(models.course), {
+      through: 'enrollments',
+    }
   };
   return user;
 };
