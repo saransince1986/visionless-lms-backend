7b4ece8d03c7a5a62787158bed7156da44dcca48
cefeboru <cefeboru@gmail.com>
2018-08-04 11:30:50 -0600

    Just started hapi migration

---
+++ src/hapi_server.js
@@ -0,0 +1,45 @@
+import Hapi from 'hapi';
+import Inert from 'inert';
+import Vision from 'vision';
+import HapiSwagger from 'hapi-swagger';
+
+import Pack from '../package.json';
+import routes from './hapi_routes/index';
+
+const init = async () => {
+  const server = await Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
+
+  const swaggerOptions = {
+    info: {
+      title: 'Bencaleth LMS API Documentation',
+      version: Pack.version,
+    },
+  };
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions,
+    },
+  ]);
+  try {
+    await server.start();
+    server.route(routes);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
