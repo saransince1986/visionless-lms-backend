b067fac93719b1b45107262fb45bc8a0e366811b
cefeboru <cefeboru@gmail.com>
2018-08-04 11:27:27 -0600

    WIP create course

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import expressJoi from 'express-joi-validator';
 
 export default function ({
   coursesRequestHandler,
+  coursesSchemaValidator,
 }) {
   const router = express.Router();
 
@@ -9,6 +11,17 @@ export default function ({
     try {
       const courses = await coursesRequestHandler.getAllCourses() || [];
       res.send(courses);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err.message);
+    }
+  });
+
+  router.post('/', expressJoi(coursesSchemaValidator.post), async (req, res) => {
+    try {
+      const { courseName, courseId } = req.params;
+      const createdCourse = await coursesRequestHandler.createCourse({ courseName, courseId });
+      res.send(createdCourse);
     } catch (err) {
       console.error(err);
       res.status(500).send(err.message);
