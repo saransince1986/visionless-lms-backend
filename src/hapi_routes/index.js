e0a210903ef83589bc03d3578c57a2ece8d22c6c
cefeboru <cefeboru@gmail.com>
2018-08-06 22:30:22 -0600

    Loading routes dynamically and created npm dev script

--- src/hapi_routes/index.js
+++ src/hapi_routes/index.js
@@ -1,12 +1,17 @@
-import userRoutes from './users';
+const normalizedPath = require('path').join(__dirname);
 
-export default [
-  {
-    method: 'GET',
-    path: '/',
-    handler() {
-      return 'Hello!';
-    },
-  },
-  ...userRoutes,
-];
+export function loadRoutes() {
+  const routes = [];
+  require('fs') // eslint-disable-line global-require
+    .readdirSync(normalizedPath)
+    .forEach((file) => {
+      if (file !== 'index.js') {
+        // eslint-disable-next-line import/no-dynamic-require, global-require
+        const routesFile = require(`./${file}`).default;
+        routesFile.forEach((route) => {
+          routes.push(route);
+        });
+      }
+    });
+  return routes;
+}
