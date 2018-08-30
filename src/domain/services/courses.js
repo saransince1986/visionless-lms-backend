93f71b4d6c21ae3b31af4f7ecdeb2f9e41ea558a
cefeboru <cefeboru@gmail.com>
2018-08-29 20:47:45 -0600

    Handling course sections creation and removal

--- src/domain/services/courses.js
+++ src/domain/services/courses.js
@@ -1,7 +1,8 @@
 export default class CoursesManagerService {
-  constructor({ courseModel, sectionModel }) {
+  constructor({ courseModel, sectionModel, courseSectionTypes }) {
     this.courseModel = courseModel;
     this.sectionModel = sectionModel;
+    this.courseSectionTypes = courseSectionTypes;
   }
 
   getAllCourses() {
@@ -13,18 +14,28 @@ export default class CoursesManagerService {
   }
 
   createCourse(course) {
-    return this.courseModel.create(course);
+    return this.courseModel.create(course).then(async (createdCourse) => {
+      createdCourse.addSection(await this.createRootSection());
+      return createdCourse;
+    });
   }
 
-  deleteById(courseId) {
-    return this.courseModel.destroy({
-      where: {
-        id: courseId,
-      },
-    });
+  async deleteById(courseId) {
+    const course = await this.getCourseById(courseId);
+    await course.setSections([]);
+    return course.destroy();
   }
 
   getCourseSections(courseId) {
     return this.getCourseById(courseId).then((course) => course.getSections());
+  }
+
+  createRootSection() {
+    return this.sectionModel.create({
+      sectionIndex: 0,
+      sectionTitle: 'root',
+      parentSection: null,
+      sectionType: this.courseSectionTypes.ROOT.code,
+    });
   }
 }
